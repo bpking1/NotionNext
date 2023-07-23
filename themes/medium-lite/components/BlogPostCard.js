dc8eb223e59330dbc6b72c455a6028e4ba0294e6
bpking1 <yihe.hejie@gmail.com>
2023-07-24 07:24:00 +0800

    update postcard display

--- themes/medium-lite/components/BlogPostCard.js
+++ themes/medium-lite/components/BlogPostCard.js
@@ -4,14 +4,14 @@ import React from 'react'
 
 const BlogPostCard = ({ post }) => {
     return (
-        <div className="w-full">
+        <div className="w-full py-1 sm:py-0">
             <Link
                 href={`${BLOG.SUB_PATH}/${post.slug}`}
                 passHref
                 className={
                     'cursor-pointer text-lg leading-tight dark:text-gray-300 hover:text-green-500 dark:hover:text-green-400'
                 }>
-                <text className='border-dotted border-b border-black'>
+                <text className='border-dotted border-b border-black dark:border-gray-300'>
                     {post.title}
                 </text>
                 <time className="text-sm text-gray-500 dark:text-gray-300 sm:text-black  py-1 ml-3 sm:float-right">{post.date?.start_date}</time>
