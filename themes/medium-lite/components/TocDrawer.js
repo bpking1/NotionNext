4a2fd33a82ceb45e6e825793611552dd284487ac
bpking1 <yihe.hejie@gmail.com>
2023-07-26 06:54:03 +0800

    sync upstream code

--- themes/medium-lite/components/TocDrawer.js
+++ themes/medium-lite/components/TocDrawer.js
@@ -14,12 +14,18 @@ const TocDrawer = ({ post, cRef }) => {
     changeTocVisible(!tocVisible)
   }
   return <>
-    {post && <>
-      <div className={(tocVisible ? 'animate__slideInRight' : ' -mr-72 animate__slideOutRight md:animate__slideInRight md:mr-0') +
-        ' overflow-y-hidden shadow-card w-60 duration-200 fixed right-1 bottom-16 z-40 rounded py-2 bg-[#f8f3e9] dark:bg-gray-600 dark:text-gray-400 text-gray-600 h-56 md:h-96'}>
-        <Catalog toc={post.toc} />
+    <div id='medium-toc-float' className='fixed top-0 right-0 z-40'>
+      {/* 侧边菜单 */}
+      <div
+        className={(tocVisible ? 'animate__slideInRight ' : ' -mr-72 animate__slideOutRight md:animate__slideInRight md:mr-0') +
+        ' overflow-y-hidden shadow-card w-60 duration-200 fixed right-1 bottom-16 rounded py-2 bg-[#f8f3e9] dark:bg-gray-600'}>
+          {post && <>
+           <div className='dark:text-gray-400 text-gray-600 h-56 md:h-96'>
+             <Catalog toc={post.toc}/>
+           </div>
+          </>}
       </div>
-    </>}
+    </div>
     {/* 背景蒙版 */}
     <div id='right-drawer-background' className={(tocVisible ? 'block md:hidden' : 'hidden') + ' fixed top-0 left-0 z-30 w-full h-full'}
       onClick={switchVisible} />
