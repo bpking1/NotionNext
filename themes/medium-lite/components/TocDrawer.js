dba5e75cabce220630a45239dfabef4af351e36a
bpking1 <yihe.hejie@gmail.com>
2023-07-25 18:26:54 +0800

    fix medium-lite toc scroll

--- themes/medium-lite/components/TocDrawer.js
+++ themes/medium-lite/components/TocDrawer.js
@@ -16,7 +16,7 @@ const TocDrawer = ({ post, cRef }) => {
   return <>
     {post && <>
       <div className={(tocVisible ? 'animate__slideInRight' : ' -mr-72 animate__slideOutRight md:animate__slideInRight md:mr-0') +
-        ' overflow-y-hidden shadow-card w-60 duration-200 fixed right-1 bottom-16 z-40 rounded py-2 bg-[#f8f3e9] dark:bg-gray-600 dark:text-gray-400 text-gray-600 h-56'}>
+        ' overflow-y-hidden shadow-card w-60 duration-200 fixed right-1 bottom-16 z-40 rounded py-2 bg-[#f8f3e9] dark:bg-gray-600 dark:text-gray-400 text-gray-600 h-56 md:h-96'}>
         <Catalog toc={post.toc} />
       </div>
     </>}
